e5592bf30c309edf2ea3d712744166229a551dbc
dryzesolo <temirbulatovamin12@gmail.com>
2026-03-13 23:22:34 +0300

    less 04

--- solutions/sem02/lesson04/task1.py
+++ solutions/sem02/lesson04/task1.py
@@ -2,16 +2,34 @@ import numpy as np
 
 
 def pad_image(image: np.ndarray, pad_size: int) -> np.ndarray:
-    # ваш код
-    return image
+    if pad_size < 1:
+        raise ValueError
+    if image.ndim == 2:
+        h, w = image.shape
+        pad_image = np.zeros((h + 2 * pad_size, w + 2 * pad_size), dtype=image.dtype)
+        pad_image[pad_size : pad_size + h, pad_size : pad_size + w] = image
+    elif image.ndim == 3:
+        h, w, c = image.shape
+        pad_image = np.zeros((h + 2 * pad_size, w + 2 * pad_size, c), dtype=image.dtype)
+        pad_image[pad_size : pad_size + h, pad_size : pad_size + w, :] = image
+    else:
+        raise ValueError
+    return pad_image
 
 
-def blur_image(
-    image: np.ndarray,
-    kernel_size: int,
-) -> np.ndarray:
-    # ваш код
-    return image
+def blur_image(image: np.ndarray, kernel_size: int) -> np.ndarray:
+    if kernel_size < 1 or kernel_size % 2 == 0:
+        raise ValueError
+    if kernel_size == 1:
+        return image
+    pad = kernel_size // 2
+    padd_image = pad_image(image, pad)
+    moved = [
+        padd_image[i : i + image.shape[0], j : j + image.shape[1]]
+        for i in range(kernel_size)
+        for j in range(kernel_size)
+    ]
+    return np.round(np.mean(moved, axis=0)).astype(image.dtype)
 
 
 if __name__ == "__main__":
--- solutions/sem02/lesson04/task2.py
+++ solutions/sem02/lesson04/task2.py
@@ -6,5 +6,18 @@ def get_dominant_color_info(
     threshold: int = 5,
 ) -> tuple[np.uint8, float]:
     # ваш код
-
-    return 0, 0
+    if threshold < 1:
+        raise ValueError("threshold must be positive")
+    image_uniq, image_count = np.unique(image, return_counts=True)
+    boollist = np.zeros(256)
+    boollist[image_uniq] = image_count
+    maxsum = 0
+    for pix in image_uniq:
+        intpix = int(pix)
+        le = max(0, intpix - threshold + 1)
+        r = min(255, intpix + threshold - 1)
+        mysum = np.sum(boollist[le : r + 1])
+        if mysum > maxsum:
+            rasp = pix
+            maxsum = mysum
+    return rasp, maxsum / image.size * 100
