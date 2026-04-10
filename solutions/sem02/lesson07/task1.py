678bb9dd3ff7b43f43a3335667d110de4028eaed
dryzesolo <temirbulatovamin12@gmail.com>
2026-04-10 22:40:12 +0300

    solution less07

--- solutions/sem02/lesson07/task1.py
+++ solutions/sem02/lesson07/task1.py
@@ -13,8 +13,58 @@ def visualize_diagrams(
     ordinates: np.ndarray,
     diagram_type: Any,
 ) -> None:
-    # ваш код
-    pass
+    if abscissa.shape != ordinates.shape:
+        raise ShapeMismatchError
+    if diagram_type not in {"box", "violin", "hist"}:
+        raise ValueError
+    grid = plt.GridSpec(4, 4, wspace=0.2, hspace=0.2)
+    figure = plt.figure(figsize=(8, 8))
+    scatter = figure.add_subplot(grid[:-1, 1:])
+    abs = figure.add_subplot(grid[:-1, 0], sharex=scatter)
+    ord = figure.add_subplot(grid[-1, 1:], sharey=scatter)
+    scatter.scatter(abscissa, ordinates, color="green", alpha=0.5)
+    if diagram_type == "box":
+        ord.boxplot(
+            ordinates,
+            patch_artist=True,
+            boxprops=dict(facecolor="red", color="lightgreen"),
+            medianprops=dict(color="black"),
+        )
+        abs.boxplot(
+            abscissa,
+            vert=False,
+            patch_artist=True,
+            boxprops=dict(facecolor="red", color="lightgreen"),
+            medianprops=dict(color="black"),
+        )
+        abs.invert_yaxis()
+        ord.invert_xaxis()
+    elif diagram_type == "hist":
+        ord.hist(abscissa, bins=100, color="lightgreen", density=True, alpha=0.5)
+        abs.hist(
+            ordinates,
+            orientation="horizontal",
+            bins=100,
+            color="lightgreen",
+            density=True,
+            alpha=0.5,
+        )
+        ord.invert_xaxis()
+        abs.invert_yaxis()
+    else:
+        vert = ord.violinplot(ordinates, showmedians=True)
+        hor = abs.violinplot(abscissa, showmedians=True, vert=False)
+        for v in [hor, vert]:
+            for body in v["bodies"]:
+                body.set_facecolor("lightgreen")
+                body.set_edgecolor("green")
+                body.set_alpha(0.5)
+            for i in v:
+                if i == "bodies":
+                    continue
+                v[i].set_edgecolor("lightgreen")
+        abs.invert_yaxis()
+        ord.invert_xaxis()
 
 
 if __name__ == "__main__":
--- solutions/sem02/lesson07/task2.py
+++ solutions/sem02/lesson07/task2.py
@@ -1,1 +1,26 @@
 # ваш код (используйте функции или классы для решения данной задачи)
+import json
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+def solve():
+    plt.style.use("ggplot")
+    info = json.load(open("data/medic_data.json", "r", encoding="utf-8"))
+    bef = np.array(info["before"])
+    aft = np.array(info["after"])
+    levels = ["I", "II", "III", "IV"]
+    befc = np.array([np.sum(bef == lev) for lev in levels])
+    aftc = np.array([np.sum(aft == lev) for lev in levels])
+    f, ax = plt.subplots(figsize=(16, 10))
+    x = np.arange(len(levels))
+    ax.bar(x - 0.5 / 2, befc, 0.5, label="Before", color="green", edgecolor="black")
+    ax.bar(x + 0.5 / 2, aftc, 0.5, label="After", color="blue", edgecolor="black")
+    ax.set_title("Mitral disease stages", fontsize=15, fontweight="bold", color="black")
+    ax.set_ylabel("Amount of people", fontsize=15, fontweight="bold", color="black")
+    ax.set_xticks(x)
+    ax.set_xticklabels(levels, fontweight="bold")
+    ax.legend()
+    plt.show()
+if __name__ == "__main__":
+    solve()
